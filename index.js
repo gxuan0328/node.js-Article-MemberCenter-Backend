3753ae192fbe05dff4ff71a82a6a0c79d7b4d15a
HSUAN <gxuan0328@gmail.com>
2021-10-26 18:54:16 +0800

    1026 api response修改完成

--- index.js
+++ index.js
@@ -3,116 +3,150 @@ const app = express();
 const cors = require('cors');
 const sql = require('mssql');
 const config = require('./dbconfig');
-// const operation = require('./operation');
 const port = 5000;
+
+
 app.use(cors());
 app.use(express.json());
 
-// app.get('/getData', (req, res) => {
-//         // connect to your database
-//         sql.connect(config, function (err) {
-//             if (err) console.log(err);
-    
-//             // create Request object
-//             var request = new sql.Request();
-               
-//             // query to the database and get the records
-//             request.query('select * from Student', function (err, recordset) {
-                
-//                 if (err) console.log(err);
-    
-//                 // send records as a response
-//                 res.send(recordset);
-                
-//             });
-//         });
-// });
+    // let pool = await sql.connect(config);
+    // let data = await pool.query();
 
-// //test api
-// app.get('/name', operation.getData);
-// app.get('/user/:name', operation.getUser);
 
-app.get('/', (req, res) => {
-    sql.connect(config, function () {
-        let request = new sql.Request();
-        request.query('select * from Articles', function (err, recordset) {
-            res.send({StatusCode: 200, Message: 'finish search article list', Data: recordset.recordset});
-        });  
-    });
+app.get('/', async(req, res) => {
+    try{
+       let pool = await sql.connect(config);
+        let data = await pool.query('select * from Articles');
+        res.send({StatusCode: 200, Message: 'finish search article list', Data: data.recordset});
+        pool.close();
+    }
+    catch(error){
+        console.log(error);
+        res.send({StatusCode: 500, Message: error, Data: {}});
+    }
 });
-app.get('/detail/:id', (req, res) => {
-    sql.connect(config, function () {
+
+app.get('/detail/:id', async(req, res) => {
+    try{
         let id = req.params.id;
-        let request = new sql.Request();
-        request.query(`select * from Articles where id=${id}`, function (err, recordset) {
-            res.send({StatusCode: 200, Message: 'finish get article', Data: recordset.recordset[0]});
-        });
-    });
-});
-app.put('/', (req, res) => {
-    sql.connect(config, function () {
-        let article = req.body;
-        console.log(article);
-        let request = new sql.Request();
-        request.query(`update Articles set title='${article.title}', author='${article.author}', content='${article.content}' where id=${article.id}`);
-        res.send({StatusCode: 200, Message: 'finish update', Data: {}});
-    });
-});
-app.delete('/:id', (req, res) => {
-    sql.connect(config, function () {
-        let id = req.params.id;
-        let request = new sql.Request();
-        request.query(`delete Articles where id=${id}`);
-        res.send({StatusCode: 200, MESSAGE:'finish delete', Data: {}});
-    });
-});
-app.post('/', (req, res) => {
-    sql.connect(config, function () {
-        let article = req.body;
-        console.log(article);
-        let request = new sql.Request();
-        request.query(`insert into Articles(title, author, content) values ('${article.title}', '${article.author}', '${article.content}') select SCOPE_IDENTITY() as id`, function (err, recordset) {
-            article.id = recordset.recordset[0].id;
-            res.send({StatusCode: 200, MESSAGE:'finish create', Data: article});
-        });
+        let pool = await sql.connect(config);
+        let data = await pool.query(`select * from Articles where id=${id}`);
+        if(data.rowsAffected[0] === 1){
+            res.send({StatusCode: 200, Message: 'finish get article', Data: data.recordset[0]});
+        }
+        else{
+            res.send({StatusCode: 404, Message: 'this article not exist', Data: {}});
+        }
+        pool.close();
+    }
+    catch(error){
+        console.log(error);
+        res.send({StatusCode: 500, Message: error, Data: {}});
+    }
 
-        
-    });
 });
-app.get('/search/:title', (req, res) => {
-    sql.connect(config, function () {
+
+app.put('/detail/:id', async(req, res) => {
+    try{
+        let article = req.body;
+        let id = Number(req.params.id);
+        let pool = await sql.connect(config);
+        if(id === article.id){
+            await pool.query(`update Articles set title='${article.title}', author='${article.author}', content='${article.content}' where id=${article.id}`);
+            res.send({StatusCode: 200, Message: 'finish update', Data: {}});
+        }
+        else{
+            res.send({StatusCode: 404, Message: 'update failed, title id is not equal to  body id', Data: {}});
+        }
+        pool.close();
+    }
+    catch(error){
+        console.log(error);
+        res.send({StatusCode: 500, Message: error, Data: {}});
+    }
+});
+
+app.delete('/:id', async(req, res) => {
+    try{
+        let id = req.params.id;
+        let pool = await sql.connect(config);
+        let data = await pool.query(`delete Articles where id=${id}`);
+        if(data.rowsAffected[0] === 1){
+            res.send({StatusCode: 200, Message:'finish delete', Data: {}});
+        }
+        else{
+            res.send({StatusCode: 404, Message:'the article isn\'t exist', Data: {}});
+        }
+        pool.close();
+    }
+    catch(error){
+        console.log(error);
+        res.send({StatusCode: 500, Message: error, Data: {}});
+    }
+});
+
+app.post('/', async(req, res) => {
+    try{
+        let article = req.body;
+        let pool = await sql.connect(config);
+        console.log(article.title !== '' && article.author !== '' && article.content !== '');
+        if(article.title !== '' && article.author !== '' && article.content !== ''){
+            await pool.query(`insert into Articles(title, author, content) values ('${article.title}', '${article.author}', '${article.content}')`);
+            res.send({StatusCode: 200, Message:'finish create', Data: {}});
+        }
+        else{
+            res.send({StatusCode: 404, Message:'create failed, missing some value', Data: article});
+        }
+        pool.close();
+    }
+    catch(error){
+        console.log(error);
+        res.send({StatusCode: 500, Message: error, Data: {}});
+    }
+});
+
+app.get('/search/:title', async(req, res) => {
+    try{
         let title = req.params.title;
-        let request = new sql.Request();
-        request.query(`select * from Articles where title like '%${title}%'`, function (err, recordset) {
-            res.send(recordset.recordset);
-            console.log(title);
-            console.log(recordset.recordset);
-            console.log({StatusCode: 200, Message: 'finish search term', Data: recordset.recordset});
-        });
-    });
+        let pool = await sql.connect(config);
+        let data = await pool.query(`select * from Articles where title like '%${title}%'`);
+        res.send(data.recordset);
+        console.log(title);
+        console.log(data.recordset);
+        console.log({StatusCode: 200, Message: 'finish search term', Data: data.recordset});
+        pool.close();
+
+    }
+    catch(error){
+        console.log(error);
+        res.send({StatusCode: 500, Message: error, Data: {}});
+    }
 });
-app.get('/getUser', (req, res) => {
-    sql.connect(config, function () {
+
+app.get('/login', async(req, res) => {
+    try{
         let username = req.query.UserName;
         let password = req.query.Password;
         let result = false;
-        console.log(username,' ',password);
-        let request = new sql.Request();
-        request.query(`select * from [User] where UserName='${username}' and Password='${password}'`, function (err, recordset) {
-            console.log(recordset);
-            if(recordset.recordset[0] != undefined) { 
+        let pool = await sql.connect(config);
+        let data = await pool.query(`select * from [User] where UserName='${username}' and Password='${password}'`);
+    
+            if(data.recordset[0] != undefined) { 
                 result=true;
                 console.log(result);
-                console.log(recordset.recordset[0]);
-                res.send({StatusCode: 200, Message: 'finish get article', Data: recordset.recordset[0]});
+                console.log(data.recordset[0]);
+                res.send({StatusCode: 200, Message: 'login success', Data: data.recordset[0]});
             }
             else{
                 console.log(result);
-                res.send({StatusCode: 400, Message: 'get article failed', Data: recordset.recordset[0]});
+                res.send({StatusCode: 404, Message: 'incorrect username or password', Data: data.recordset[0]});
             }
-            
-        });
-    });
+        pool.close();
+    }
+    catch(error){
+        console.log(error);
+        res.send({StatusCode: 500, Message: error, Data: {}});
+    }
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
